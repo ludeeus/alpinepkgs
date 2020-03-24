ec2f9a4a7390078009d86eeab4a493c9c3852e2d
ludeeus <joasoe@gmail.com>
2020-03-24 10:21:18 +0000

    Fix scraping issues

--- alpinepkgs/packages.py
+++ alpinepkgs/packages.py
@@ -24,60 +24,68 @@ def get_package(package=None, branch=BRANCHES[-2]):
         _LOGGER.error("Only %s are supported as the branch!", BRANCHES)
         return None
 
-    data = {'package': package, 'branch': branch}
+    data = {"package": package, "branch": branch}
 
     versions = []
 
-    if '+' in package:
-        package = package.replace('+', '%2B')
+    if "+" in package:
+        package = package.replace("+", "%2B")
 
     url = BASE_URL.format(package, branch)
 
     get = requests.get(url)
 
-    packages = get.text.replace('\n', '')
-    packages = packages.split('<tbody>')[1]
-    packages = packages.split('</tbody>')[0]
+    packages = get.text.replace("\n", "")
+    packages = packages.split("<tbody>")[1]
+    packages = packages.split("</tbody>")[0]
     packages = packages.split('<td class="package">')
 
     for architecture in packages:
-        if architecture.replace(' ', '') != '<tr>':
+        if architecture.replace(" ", "") != "<tr>":
             try:
                 arch = architecture.split('<td class="arch">')[1]
-                arch = arch.split('</td>')[0]
+                arch = arch.split("</td>")[0]
+                arch = arch.split(">")[1]
+                arch = arch.split("<")[0]
+                arch = arch.strip()
 
                 version = architecture.split('<td class="version">')[1]
-                version = version.split('</td>')[0]
-                if branch == 'edge':
-                    version = version.split('>')[2]
-                    version = version.split('<')[0]
+                version = version.split("</td>")[0]
+                if branch == "edge":
+                    version = version.split(">")[2]
+                    version = version.split("<")[0]
 
                 if version not in versions:
                     versions.append(version)
 
                 date = architecture.split('<td class="bdate">')[1]
-                date = date.split('</td>')[0]
-                date = date.split(' ')[0]
+                date = date.split("</td>")[0]
+                date = date.split(" ")[0]
 
                 licence = architecture.split('<td class="license">')[1]
-                licence = licence.split('</td>')[0]
+                licence = licence.split("</td>")[0]
 
                 maintainer = architecture.split('<td class="maintainer">')[1]
-                maintainer = maintainer.split('</td>')[0]
+                maintainer = maintainer.split("</td>")[0]
+                maintainer = maintainer.split(">")[1]
+                maintainer = maintainer.split("<")[0]
+                maintainer = maintainer.strip()
 
                 url = architecture.split('<td class="url">')[1]
-                url = url.split('</td>')[0]
+                url = url.split("</td>")[0]
                 url = url.split('"')[1]
-                url = url.replace('&#x2F;', '/')
+                url = url.replace("&#x2F;", "/")
 
-                data[arch] = {'version': version,
-                              'date': date,
-                              'licence': licence,
-                              'maintainer': maintainer,
-                              'url': url}
+                data[arch] = {
+                    "version": version,
+                    "date": date,
+                    "licence": licence,
+                    "maintainer": maintainer,
+                    "url": url,
+                }
             except IndexError:
                 _LOGGER.error("Package %s not found on %s", package, branch)
 
-    data['versions'] = versions
+    data["versions"] = versions
 
     return data
