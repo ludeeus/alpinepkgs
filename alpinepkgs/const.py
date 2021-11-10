504786f79915764ae21f7bbedb55b8a10e149682
Joakim Sørensen <hi@ludeeus.dev>
2021-11-10 18:35:24 +0100

    Add 3.13 and 3.14 (#2)

--- alpinepkgs/const.py
+++ alpinepkgs/const.py
@@ -1,13 +1,17 @@
 """Constants."""
-BASE_URL = 'https://pkgs.alpinelinux.org/packages?name={}&branch={}'
-BRANCHES = ['v3.3',
-            'v3.4',
-            'v3.5',
-            'v3.6',
-            'v3.7',
-            'v3.8',
-            'v3.9',
-            'v3.10',
-            'v3.11',
-            'v3.12',
-            'edge']
+BASE_URL = "https://pkgs.alpinelinux.org/packages?name={}&branch={}"
+BRANCHES = [
+    "v3.3",
+    "v3.4",
+    "v3.5",
+    "v3.6",
+    "v3.7",
+    "v3.8",
+    "v3.9",
+    "v3.10",
+    "v3.11",
+    "v3.12",
+    "v3.13",
+    "v3.14",
+    "edge",
+]
