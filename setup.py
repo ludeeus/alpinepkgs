299b1b0c32a5a2abaccb17db13a11d9c376f6bf0
ludeeus <ludeeus@ludeeus.dev>
2022-11-06 10:52:14 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="alpinepkgs",
-    version="1.1.5",
+    version="1.1.6",
     author="Joakim Sorensen",
     author_email="ludeeus@gmail.com",
     description="",
