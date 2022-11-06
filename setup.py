94b69943f90172ad92e2d25bf08b1c08a8a1d11f
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-11-06 12:11:19 +0100

    Remove the CLI (#5)

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 """Setup configuration."""
 import setuptools
 
-with open("README.md", "r") as fh:
-    LONG = fh.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    README_CONTENTS = fh.read()
+
 setuptools.setup(
     name="alpinepkgs",
     version="1.1.6",
     author="Joakim Sorensen",
     author_email="ludeeus@gmail.com",
     description="",
-    long_description=LONG,
-    install_requires=["requests", "click"],
+    long_description=README_CONTENTS,
+    install_requires=["requests"],
     long_description_content_type="text/markdown",
     url="https://github.com/ludeeus/alpinepkgs",
     packages=setuptools.find_packages(),
@@ -19,5 +20,4 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
-    entry_points={"console_scripts": ["alpinepkgs = alpinepkgs.cli:cli"]},
 )
